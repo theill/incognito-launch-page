d7ff42a0917169336831538b09aeb822ee8a727d
Peter Theill <peter@theill.com>
2010-10-03 11:25:53 +0200

    removed login page since it's not used yet. updated twistori to have a dark background and optimized javascript

--- js/core.js
+++ js/core.js
@@ -1,3 +1,54 @@
 function setUniqueIdentifier(uuid) {
 	document.getElementById("user_uuid").value = uuid;
 }
+
+var sentences = ["i love", "i hate", "i think", "i believe", "i feel", "i wish"];
+
+var casualBrowsing = {
+	sentence: sentences[Math.floor(Math.random()*sentences.length)],
+	
+	randomChange: function() {
+		this.sentence = sentences[Math.floor(Math.random() * sentences.length)];
+		
+		$("#available-sentences li").removeClass("selected");
+		$("#available-sentences ul li." + this.sentence.replace(/ /g, '-')).addClass("selected");
+		
+		this.setup();
+	},
+	
+	sentenceClassName: function() {
+		return this.sentence.replace(/ /g, '-');
+	},
+	
+	setup: function() {
+		var cs = this;
+		
+		var sentenceRegEx = new RegExp(cs.sentence, "gi");
+
+		$("#twitterSearch").liveTwitter('"' + cs.sentence + '"', { limit: 5, rate: 3000, lang: 'en', timeLinks: false }, function(container, newCount) {
+			texts = $(container).find(".tweet p.text");
+			$(texts).each(function(cnt, p) {
+				$(p).html($(p).html().replace(sentenceRegEx, "<span class=\"" + cs.sentenceClassName() + "\">" + cs.sentence + "</span>"));
+			});
+		});
+		
+		window.setTimeout('casualBrowsing.randomChange()', 5000);
+	}
+};
+
+$(function() {
+	$("#available-sentences").html("<ul>" + $.map(sentences, function(ia, idx) {
+		return "<li data-sentence=\"" + ia + "\" class='" + ia.replace(/ /g, '-') + "'><span>" + ia + "</span></li>"
+		}).join("") + "</ul>");
+	
+	$("#available-sentences ul li").click(function() {
+		$("#available-sentences li").removeClass("selected");
+		
+		$(this).addClass("selected");
+		
+		casualBrowsing.sentence = $(this).attr("data-sentence");
+		casualBrowsing.setup();
+	});
+	
+	casualBrowsing.randomChange();
+});
